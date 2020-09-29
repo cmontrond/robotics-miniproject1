bff46785763351c87294059e1bade2083f613887
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-09-29 19:03:44 -0400

    fix left and right

--- miniProject.go
+++ miniProject.go
@@ -26,7 +26,7 @@ func stop(gopigo3 *g.Driver) {
 }
 
 func turnRight(gopigo3 *g.Driver) {
-	err := gopigo3.SetMotorPosition(g.MOTOR_RIGHT, -30)
+	err := gopigo3.SetMotorPosition(g.MOTOR_LEFT, -30)
 	if err != nil {
 		fmt.Errorf("Error turning right wheel %+v", err)
 	}
