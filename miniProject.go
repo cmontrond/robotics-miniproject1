91e96ef640d27aacff5c21a475d2f912c82cec88
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-09-30 15:42:09 -0400

    introduce difference constant

--- miniProject.go
+++ miniProject.go
@@ -142,73 +142,76 @@ func blinkLED(gopigo3 *g.Driver) {
 
 func robotRunLoop(gopigo3 *g.Driver, leftLightSensor *aio.GroveLightSensorDriver, rightLightSensor *aio.GroveLightSensorDriver) {
 
-	robotStopped := false
+	//robotStopped := false
 
 	for {
 
-		// Read value from the left light sensor
-		leftLightSensorVal, err := leftLightSensor.Read()
+		right(gopigo3)
+		time.Sleep(time.Second * 3)
 
-		if err != nil {
-			fmt.Errorf("Error reading sensor %+v", err)
-		}
-
-		// read value from the right light sensor
-		rightLightSensorVal, err := rightLightSensor.Read()
-
-		if err != nil {
-			fmt.Errorf("Error reading sensor %+v", err)
-		}
-
-		fmt.Println("Right Light Value is ", rightLightSensorVal)
-		fmt.Println("Left Light Value is ", leftLightSensorVal)
-
-		// Stop the Robot if too close to the light
-		if (rightLightSensorVal >= LIGHT_TOO_CLOSE) && (leftLightSensorVal >= LIGHT_TOO_CLOSE) {
-			stop(gopigo3)
-			robotStopped = true
-			blinkLED(gopigo3)
-		}
-
-		if robotStopped == false {
-
-			rightLeftDifference := rightLightSensorVal - leftLightSensorVal
-			leftRightDifference := leftLightSensorVal - rightLightSensorVal
-
-			// If the light comes from the right, turn right and move forward
-			if (rightLightSensorVal > leftLightSensorVal) && (rightLightSensorVal >= LIGHT_IN_REACH) && (rightLeftDifference >= DIFFERENCE) {
-
-				//turnRight(gopigo3)
-				//time.Sleep(time.Second)
-				//turnMove(gopigo3)
-				//time.Sleep(time.Millisecond * 200)
-
-				right(gopigo3)
-				time.Sleep(time.Second)
-
-				// If the light comes from the left, turn left and move forward
-			} else if (leftLightSensorVal > rightLightSensorVal) && (leftLightSensorVal >= LIGHT_IN_REACH) && (leftRightDifference >= DIFFERENCE) {
-
-				//turnLeft(gopigo3)
-				//time.Sleep(time.Second)
-				//turnMove(gopigo3)
-				//time.Sleep(time.Millisecond * 200)
-
-				left(gopigo3)
-				time.Sleep(time.Second)
-
-			} else if (rightLightSensorVal >= LIGHT_IN_REACH) && (leftLightSensorVal >= LIGHT_IN_REACH) {
-
-				//moveForward(gopigo3)
-				//time.Sleep(time.Second)
-
-				forward(gopigo3)
-				time.Sleep(time.Second)
-
-			} else {
-				stop(gopigo3)
-			}
-		}
+		//// Read value from the left light sensor
+		//leftLightSensorVal, err := leftLightSensor.Read()
+		//
+		//if err != nil {
+		//	fmt.Errorf("Error reading sensor %+v", err)
+		//}
+		//
+		//// read value from the right light sensor
+		//rightLightSensorVal, err := rightLightSensor.Read()
+		//
+		//if err != nil {
+		//	fmt.Errorf("Error reading sensor %+v", err)
+		//}
+		//
+		//fmt.Println("Right Light Value is ", rightLightSensorVal)
+		//fmt.Println("Left Light Value is ", leftLightSensorVal)
+		//
+		//// Stop the Robot if too close to the light
+		//if (rightLightSensorVal >= LIGHT_TOO_CLOSE) && (leftLightSensorVal >= LIGHT_TOO_CLOSE) {
+		//	stop(gopigo3)
+		//	robotStopped = true
+		//	blinkLED(gopigo3)
+		//}
+		//
+		//if robotStopped == false {
+		//
+		//	rightLeftDifference := rightLightSensorVal - leftLightSensorVal
+		//	leftRightDifference := leftLightSensorVal - rightLightSensorVal
+		//
+		//	// If the light comes from the right, turn right and move forward
+		//	if (rightLightSensorVal > leftLightSensorVal) && (rightLightSensorVal >= LIGHT_IN_REACH) && (rightLeftDifference >= DIFFERENCE) {
+		//
+		//		//turnRight(gopigo3)
+		//		//time.Sleep(time.Second)
+		//		//turnMove(gopigo3)
+		//		//time.Sleep(time.Millisecond * 200)
+		//
+		//		right(gopigo3)
+		//		time.Sleep(time.Second)
+		//
+		//		// If the light comes from the left, turn left and move forward
+		//	} else if (leftLightSensorVal > rightLightSensorVal) && (leftLightSensorVal >= LIGHT_IN_REACH) && (leftRightDifference >= DIFFERENCE) {
+		//
+		//		//turnLeft(gopigo3)
+		//		//time.Sleep(time.Second)
+		//		//turnMove(gopigo3)
+		//		//time.Sleep(time.Millisecond * 200)
+		//
+		//		left(gopigo3)
+		//		time.Sleep(time.Second)
+		//
+		//	} else if (rightLightSensorVal >= LIGHT_IN_REACH) && (leftLightSensorVal >= LIGHT_IN_REACH) {
+		//
+		//		//moveForward(gopigo3)
+		//		//time.Sleep(time.Second)
+		//
+		//		forward(gopigo3)
+		//		time.Sleep(time.Second)
+		//
+		//	} else {
+		//		stop(gopigo3)
+		//	}
+		//}
 	}
 }
 
